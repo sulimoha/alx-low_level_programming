643acae1d1b8292a66e55604598456d09dbaaece
Mohamed Suliman <sulimmoh@gmail.com>
2022-06-30 05:25:56 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -12,6 +12,9 @@ int main(void)
  * puts - prints a string using puts
  * @Programming is like building a multilingual puzzle followed by a new line:
  * message
+ *
+ * Prints a message
+ * Return a message
  */
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
