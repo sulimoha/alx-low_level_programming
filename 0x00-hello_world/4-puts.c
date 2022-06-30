bb7f860280c393bb857d842e589bf1bbee6924bd
Mohamed Suliman <sulimmoh@gmail.com>
2022-06-29 21:13:10 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -17,6 +17,6 @@ int main(void)
  * @arg: Programming is like building a multilingual puzzle\
  * followed by a new line
  */
-	puts("Programming is like building a multilingual puzzle);
+	puts(""Programming is like building a multilingual puzzle");
 	return (0);
 }
