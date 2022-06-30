46d0123fbaa09befde840b2e16c2204cb4bc292c
Mohamed Suliman <sulimmoh@gmail.com>
2022-06-29 21:08:20 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,22 @@
+/**
+ * This is a C program that prints exactly \"Programming is like building a
+ * multilingual puzzle, followed by a new line
+ */
+#include <stdio.h>
+/**
+ * main function
+ * @parameter: no parameters
+ *
+ * Entry point
+ * returns zero
+ */
+int main(void)
+{
+/**
+ * puts - prints a string using puts
+ * @arg: Programming is like building a multilingual puzzle\
+ * followed by a new line
+ */
+	puts("Programming is like building a multilingual puzzle.\n");
+	return (0);
+}
