1dd1bf97f10d3511c45a7e897ed8b43b73c07087
Mohamed Suliman <sulimmoh@gmail.com>
2022-06-30 16:39:21 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,21 +8,31 @@
  */
 int main(void)
 {
-	char c;
-	int i;
-	double d;
-	float f;
+	char c = sizeof(c);
+	int i = sizeof(i);
+	float f = sizeof(f);
+	char mf[] = "Size of a float: %d byte(s)\n";
+	char mli[] = "Size of a long int: %d byte(s)\n";
+	char mi[] = "Size of an int: %d byte(s)\n";
+	char mc[] = "Size of an char: %d byte(s)\n";
+	char mlli[] = "Size of a long long int: %d byte(s)\n";
 /**
- * printf - prints a stringize of data typr in byte(s)
- * @byte(s\n\":message
+ * printf - prints a string size of data typr in byte(s)
+ * @c: c
+ * @mc: messag
+ * @mi: message
+ * @mli: messagel
+ * @mlli: messagell
+ * @mf: messagef
+ * @sizeof(c): sizeof(c)
  *
  * Prints a message
  * Return a message
- */
-	printf("Size of a char: ", sizeof(c), "byte(s)\n");
-	printf("Size of an int: byte(s)\n", sizeof(i));
-	printf("Size of a long int: %d byte(s)\n", sizeof((long)i));
-	printf("Size of a long long int: %d byte(s)\n", sizeof((long long)i));
-	printf("Size of a float: %d byte(s)\n", sizeof(f));
+*/
+	printf(mc, c);
+	printf(mi, i);
+	printf(mli, sizeof((long)i));
+	printf(mlli, sizeof((long long)i));
+	printf(mf, sizeof(f));
 	return (0);
 }
