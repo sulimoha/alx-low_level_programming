7dd8ead699e5688f31713ab3aafc0e9fdc0a87d8
Mohamed Suliman <sulimmoh@gmail.com>
2022-06-30 05:53:12 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * @void: no parameters
+ *
+ * Entry point
+ * Return: 0
+ */
+int main(void)
+{
+	char message[] = "\"with proper grammar, but the outcome is a piece of art,";
+/**
+ * printf - prints a string using printf
+ * @message: message
+ *
+ * Prints a message
+ * Return a message
+ */
+	printf(message);
+	return (0);
+}
