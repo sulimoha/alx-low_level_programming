0a32cc0535b624b4dd6921d2ef52916922be0b58
Mohamed Suliman <sulimmoh@gmail.com>
2022-07-01 08:10:50 -0700

    6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -12,9 +12,9 @@ int main(void)
 {
 	int num;
 
-	for (num = 0; num <= 9; num++)
+	for (num = '0'; num <= '9'; num++)
 	{
-		putchar((char)num);
+		putchar(num);
 	}
 		putchar('\n');
 		return (0);
