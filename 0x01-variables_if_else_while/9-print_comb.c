9da4067ac18146e73fda317e249ea135e8a8c424
Mohamed Suliman <sulimmoh@gmail.com>
2022-07-01 12:32:36 -0700

    9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -15,11 +15,12 @@ int main(void)
 	for (num = '0'; num <= '9'; num++)
 	{
 		putchar(num);
-		if (num < 9)
+		if (num == 9)
 		{
-			putchar(',');
-			putchar(' ');
+			return;
 		}
+		putchar(',');
+		putchar(' ');
 	}
 		putchar('\n');
 		return (0);
