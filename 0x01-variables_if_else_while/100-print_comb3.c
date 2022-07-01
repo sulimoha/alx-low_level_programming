42ce617527a60c2cd5088da56f3dc72a6dfd91cd
Mohamed Suliman <sulimmoh@gmail.com>
2022-07-01 12:48:39 -0700

    10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -15,13 +15,13 @@ int main(void)
 	for (num = '0'; num <= '9'; num++)
 	{
 		putchar(num);
-		num+ = 1;
+		num+= 1;
 		putchar(num);
 		if (num == '9')
 		{
 			break;
 		}
-		num- = 1;
+		num-= 1;
 		putchar(',');
 		putchar(' ');
 	}
