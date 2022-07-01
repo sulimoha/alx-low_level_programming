07bfa4ec60d6b1f65063bd81f5582a181f3385b7
Mohamed Suliman <sulimmoh@gmail.com>
2022-07-01 07:56:32 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+/* betty style doc for function main goes there */
+/**
+ *main - main
+ *
+ * Return: 0
+*/
+int main(void)
+{
+	int num;
+
+	for (num = '0'; num <= '9'; num++)
+	{
+		puts(num);
+	}
+		putchar('\n');
+		return (0);
+}
