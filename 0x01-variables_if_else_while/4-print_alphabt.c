4169a5908aac59416c77cb204fa1722ff6abb97b
Mohamed Suliman <sulimmoh@gmail.com>
2022-07-01 07:40:11 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+/* betty style doc for function main goes there */
+/**
+ *main - main
+ *
+ * Return: 0
+*/
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		while (ch != 'q' || ch != 'e')
+		{
+		putchar(ch);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
