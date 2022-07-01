f31517ad7cc25faa881a87a40b7b3f80f73bb4de
Mohamed Suliman <sulimmoh@gmail.com>
2022-07-01 12:41:12 -0700

    10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+/* betty style doc for function main goes there */
+/**
+ *main - main
+ *
+ * Return: 0
+*/
+int main(void)
+{
+	int num;
+
+	for (num = '0'; num <= '9'; num++)
+	{
+		putchar(num);
+		putchar(num);
+		if (num == '9')
+		{
+			break;
+		}
+		putchar(',');
+		putchar(' ');
+	}
+		putchar('\n');
+		return (0);
+}
