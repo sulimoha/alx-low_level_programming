c0032bb5bd814221d2b9578a2db478f2a77bca43
Mohamed Suliman <sulimmoh@gmail.com>
2022-07-01 19:20:27 -0700

    12

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,38 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+/* betty style doc for function main goes there */
+/**
+ *main - main
+ *
+ * Return: 0
+*/
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		for (j = '0'; j <= '9'; j++)
+		{
+			if (i < j)
+			{
+				putchar(i);
+				putchar(i);
+				putchar(' ');
+				putchar(i);
+				putchar(j);
+				if (i == '8')
+				{
+					break;
+				}
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
