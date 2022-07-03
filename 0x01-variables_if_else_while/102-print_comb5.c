fe14e72432d0d4459253d9a2cbb06aacdc516e1f
Mohamed Suliman <sulimmoh@gmail.com>
2022-07-03 08:48:58 -0700

    12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -21,7 +21,7 @@ int main(void)
 		{
 			for (k = '0'; k <= '9'; k++)
 			{
-				for (l = '0'; l < '9'; l++)
+				for (l = '0'; l <= '9'; l++)
 				{
 					if ((i == j && j == l && l == k)
 							|| (i == k && j == l))
