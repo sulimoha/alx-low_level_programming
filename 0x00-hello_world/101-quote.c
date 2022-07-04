963bd2c7d2b7984f777c2ede380316bdd13c4828
Mohamed Suliman <sulimmoh@gmail.com>
2022-07-04 04:45:56 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,7 +8,7 @@
 int main(void)
 {
 	int c;
-	c = system ("var_s = "and that piece of art is useful|\" - Dora Korpar, 2015-10-19");
+	c = system ("var_s = 'and that piece of art is useful\" - Dora Korpar, 2015-10-19'");
 	c = system ("echo $var_s");
 	c = 1;
 	return (c);
