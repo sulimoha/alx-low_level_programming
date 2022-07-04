2dd1efb94115972e6c033eca46fad3742ea1f7a6
Mohamed Suliman <sulimmoh@gmail.com>
2022-07-03 19:35:53 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,12 +14,13 @@ int main(void)
 	int b = sizeof(f);
 	char c;
 	int e = sizeof(c);
-	
 	char n[] = "Size of a long long int: %d byte(s)\n";
 	char o[] = "Size of a float: %d byte(s)\n";
 	char l[] = "Size of an int: %d byte(s)\n";
 	char k[] = "Size of a char: %d byte(s)\n";
 	char m[] = "Size of a long int: %d byte(s)\n";
+
+
 	printf("Size of a char: %d byte(s)\n", sizeof(c));
 	printf("Size of an int: %d byte(s)\n", sizeof(i));
 	printf("Size of a long int: %d byte(s)\n", sizeof((long)i));
