596d83e8f43787bb2e7c9d6c6ab008923067c1e2
Mohamed Suliman <sulimmoh@gmail.com>
2022-07-04 03:18:57 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,10 +8,8 @@
 int main(void)
 {
 	int c;
-	
-	
-	
-	c = system("echo 'and that piece of art is useful\" - Dora Korpar, 2015-10-19'");
+	c = system("echo 'and that piece of art is useful
+			\" - Dora Korpar, 2015-10-19'");
 	c = 1;
 	return (c);
 }
