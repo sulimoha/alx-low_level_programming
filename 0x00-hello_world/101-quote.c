9afea4883507e78caa8cb83f354e6980d6b317d6
Mohamed Suliman <sulimmoh@gmail.com>
2022-07-04 03:10:35 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+
+/**
+ * main - main
+ *
+ * Returns: c
+ */ 
+int main(void)
+{
+	int c;
+	
+	c = system("echo 'and that piece of art is useful\" - Dora Korpar, 2015-10-19'");
+	return (c);
+}
