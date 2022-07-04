361d0d7f58b2bdafa93a165745bc53d47be6efc7
Mohamed Suliman <sulimmoh@gmail.com>
2022-07-04 03:48:46 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,7 +8,7 @@
 int main(void)
 {
 	int c;
-	char [] s = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	char s[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
 	c = system (" echo s")
 	c = 1;
 	return (c);
