9295795c15ffc722a92dc22972995802b10d5d3b
Mohamed Suliman <sulimmoh@gmail.com>
2022-07-05 14:42:57 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - entry point
+ * @void: no parameters
+ *
+ * Return: 0
+*/
+
+int main(void)
+{
+	for (char i = 'a'; i <= 'z'; i++){
+		_putchar(i);
+	}
+		_putchar('\n');
+		return (0);
+}
