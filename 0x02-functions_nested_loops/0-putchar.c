9e3dc18c5cd5206b1511d337495cfccebbcd0523
Mohamed Suliman <sulimmoh@gmail.com>
2022-07-05 14:29:58 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include "main.h" 
+#include "main.h"
 /**
  * main - entry point
  * @void: no parameters
@@ -17,6 +17,5 @@ int main(void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
-			
 	return (0);
 }
