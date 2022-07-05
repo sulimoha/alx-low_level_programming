2a7e885000afafdf7ca201073fd4967ff7ad0868
Mohamed Suliman <sulimmoh@gmail.com>
2022-07-05 16:42:26 -0700

    3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ *  _islower - entry point
+ * @void: no parameters
+ *
+ * Return: 1 if c is lower case and 0 if it is not
+*/
+
+int _islower(int c);
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		if (i == c)
+		{
+			return (1);
+		}
+		else 
+		{
+			return (0);
+		}
+	}
+}
