06809d5507a6c5903bc3d6d5c3fac02186d021e6
Mohamed Suliman <sulimmoh@gmail.com>
2022-07-05 22:45:16 -0700

    11

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio>
+/**
+ * print_to_98 - Entry point
+ * @n: int
+ *
+ * Return: prints natural numbers
+ */
+
+void print_to_98(int n)
+{
+	int i = n;
+
+	for (i = n; i <= 98; i++)
+	{
+		printf(i);
+		if (i != 98)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+}
