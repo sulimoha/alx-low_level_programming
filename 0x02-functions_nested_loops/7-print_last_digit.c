71433bcf7576c28027c803e647bac9411d9fd312
Mohamed Suliman <sulimmoh@gmail.com>
2022-07-05 17:32:09 -0700

    7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_last_digit- entry point
+ * @n: input int
+ *
+ * Return: value of last digit
+*/
+
+int print_last_digit(int n)
+{
+	int d = n % 10;
+	return (d);
+}
