cf74cde98705e3c0fc947e224bad4aac75915695
Mohamed Suliman <sulimmoh@gmail.com>
2022-07-05 23:23:31 -0700

    11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -16,7 +16,7 @@ void print_to_98(int n)
 		for (i = n; i <= 98; i++)
 		{
 			printf("%d", i);
-			if (i != 98)
+			if (i < 98)
 			{
 				putchar(',');
 				putchar(' ');
@@ -25,15 +25,19 @@ void print_to_98(int n)
 	}
 	if (i > 98)
 	{
-		for (i = n; i > 98; i--)
+		for (i = n; i >= 98; i--)
 		{
 			printf("%d", i);
-			if (i != 98)
+			if (i > 98)
 			{
 				putchar(',');
 				putchar(' ');
 			}
 		}
+	}
+	if (i == 98)
+	{
+		printf("%d", i)
 	}
 	putchar('\n');
 }
