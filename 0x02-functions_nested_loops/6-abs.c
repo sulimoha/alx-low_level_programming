ea94d94527369da805e5f2bccaa4ee2a4b84c504
Mohamed Suliman <sulimmoh@gmail.com>
2022-07-05 17:28:00 -0700

    6

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,20 +1,20 @@
 #include "main.h"
 /**
  * _abs- entry point
- * @int: input int
+ * @n: input int
  *
  * Return: abs value
 */
 
 int _abs(int n)
 {
-	if (n > 0 )
+	if (n > 0)
 	{
 		n = n;
 	}
 	else
 	{
-		n = -1*n;
+		n = -1 * n;
 	}
 	return (n);
 }
