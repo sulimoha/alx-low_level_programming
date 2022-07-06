d62d2a2099b6281f4e97f89bf76aec9b276e6f04
Mohamed Suliman <sulimmoh@gmail.com>
2022-07-05 22:07:07 -0700

    9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,56 @@
+#include "main.h"
+/**
+ * times_table- entry point
+ * @void: void
+ *
+ * Return: times_table
+ */
+
+void times_table(void)
+{
+	int i;
+	int j;
+	int p;
+	int w;
+	int r;
+	
+	for (i = 0; i<= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			p = i * j;
+			r = p % 10;
+			w = p / 10;
+			if (p <= 9)
+			{
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(p + '0');
+				if (j == 9)
+				{
+					continue;
+				}
+				else
+				{
+					_putchar(',');
+				}
+			}
+			else
+			{
+				_putchar(' ');
+				_putchar(w + '0');
+				_putchar(r + '0');
+				if (j == 9)
+				{
+					continue;
+				}
+				else
+				{
+					_putchar(',');
+				}
+			}
+
+		}
+		_putchar('\n');
+	}
+}
