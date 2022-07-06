6aef2312be9d2d4334ea47e68c2f094000948ffc
Mohamed Suliman <sulimmoh@gmail.com>
2022-07-05 17:13:49 -0700

    5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * print_sign - entry point
+ * @c: input charachter
+ *
+ * Return: 1 and prints + if n > 0 , returns 0 and prints 0 if n is 0, returns
+ * -1 and prints - if n < 0
+*/
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{ 
+		_putchar('+');
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar('-')
+		return (-1);
+	}
+	else 
+	{
+		_putchar('0');
+		return (0);
+	}
+}
