0dc54018b05d14109c76dc43f4f3353d13d09c75
Mohamed Suliman <sulimmoh@gmail.com>
2022-07-07 14:27:42 -0700

    5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -6,16 +6,33 @@
 */
 void more_numbers(void)
 {
-	char num;
+	char num = '0';
 	int i;
+	int j;
+	int swtch = 1;
+	char x;
 
 	for (i = 0; i < 10; i++)
 	{
-		for (num = '0'; num <= '14'; num++)
+		for (j = 0; j < 20; j++)
 		{
-			_putchar(num);
+			if (j <= 9)
+			{
+				num = j + '0';
+				x = num;
+			}
+			else if (swtch == 1)
+			{
+				x = '1';
+				swtch = swtch * -1;
+			}
+			else
+			{
+				x = (j / 2 - 5) + '0';
+				swtch = swtch * -1;
+			}
+			_putchar(x);
 		}
 		_putchar('\n');
 	}
-	_putchar('\n');
 }
