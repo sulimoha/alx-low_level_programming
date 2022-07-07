383991fb8e2d204de9bf61533e6304fd7d66e03e
Mohamed Suliman <sulimmoh@gmail.com>
2022-07-07 09:44:01 -0700

    3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *print_numbers - print numbers
+ *
+ * Return:void
+*/
+void print_numbers(void)
+{
+	int num;
+
+	for (num = '0'; num <= '9'; num++)
+	{
+		_putchar(num);
+	}
+		_putchar('\n');
+		return (0);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -8,6 +8,7 @@ void print_alphabet_x10(void);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
+void print_numbers(void);
 int _islower(int c);
 int _isalpha(int c);
 int print_sign(int n);
