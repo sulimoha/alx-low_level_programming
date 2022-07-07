d71a47329b9296cb2d7dc582a139a9ef46564d93
Mohamed Suliman <sulimmoh@gmail.com>
2022-07-07 16:22:08 -0700

    7

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -18,14 +18,13 @@ void print_diagonal(int n)
 		}
 		else
 		{
-			_putchar(' ');
 			_putchar('\\');
+			_putchar(' ');
 			_putchar('\n');
 			for (j = 0; j < i + 1; j++)
 			{
 				_putchar(' ');
 			}
-			_putchar('\n');
 		}
 	}
 }
