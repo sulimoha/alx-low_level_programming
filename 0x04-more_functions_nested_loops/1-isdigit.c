3354deb548bd19fd5666b280b9706175439d87f8
Mohamed Suliman <sulimmoh@gmail.com>
2022-07-07 09:14:41 -0700

    1

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *  _isdigit - entry point
+ * @c: input charachter
+ *
+ * Return: 1 if c is upper case and 0 if it is not
+*/
+
+int _isdigit(int c)
+{
+	if (c >= 49 && c <= 57)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
