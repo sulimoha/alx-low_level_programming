73b06869ff2c0300535db2a1b590f3694066c8a2
Mohamed Suliman <sulimmoh@gmail.com>
2022-07-07 10:47:35 -0700

    5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ *more_numbers - print numbers
+ *
+ * Return:void
+*/
+void more_numbers(void)
+{
+	char num;
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (num = '0'; num <= '14'; num++)
+		{
+			_putchar(num);
+		}
+		_putchar('\n');
+	}
+}
