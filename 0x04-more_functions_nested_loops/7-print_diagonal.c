7fbccea8b1171240e3f86a31a518191c9ad730ef
Mohamed Suliman <sulimmoh@gmail.com>
2022-07-07 16:53:34 -0700

    7

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -9,24 +9,19 @@ void print_diagonal(int n)
 	int i;
 	int j;
 
-	for (i = 0; i < n; i++)
+	if (n <= 0) {
+		_putchar('\n');
+	}
+	else
 	{
-		if (n <= 0)
+		for (i = 0; i < n; i++)
 		{
-			_putchar('\n');
-			break;
-		}
-		else
-		{
-			_putchar(' ');
-			_putchar('\\');
-			_putchar(' ');
-			_putchar('\n');
-			for (j = 0; j < i + 1; j++)
+			for (j = 0; j < i; j++)
 			{
 				_putchar(' ');
 			}
+			_putchar('\\');
+			_putchar('\n');
 		}
 	}
-	_putchar('\n');
 }
