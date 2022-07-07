d94b406d5b2651bafce053473bdb3d14d05c2851
Mohamed Suliman <sulimmoh@gmail.com>
2022-07-07 09:31:58 -0700

    2

---
+++ 0x04-more_functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * add - entry point
+ * @a: input int
+ * @b: input int
+ *
+ * Return: add value
+*/
+
+int add(int a, int b)
+{
+	int sum;
+
+	sum = a + b;
+	return (sum);
+}
---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * mul - entry point
+ * @a: input int
+ * @b: input int
+ *
+ * Return: mul value
+*/
+
+int mul(int a, int b)
+{
+	int mul;
+
+	mul = a * b;
+	return (mul);
+}
