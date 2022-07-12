7ca8fb1a0aac1ff650f6bdb352db4af467bf1b06
Mohamed Suliman <sulimmoh@gmail.com>
2022-07-11 18:02:16 -0700

    3

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -8,10 +8,10 @@
 */
 void _puts(char *str)
 {
-	char i;
+	int i;
 
-	for (i = '0'; i < strlen(str); i++)
+	for (i = 0; i < strlen(str); i++)
 	{
-		_putchar((*(str + i)));
+		_putchar(str[i]);
 	}
 }
