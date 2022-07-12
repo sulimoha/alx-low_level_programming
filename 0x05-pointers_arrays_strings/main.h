422441e61dc142be3d8bf9195a6ff471bcf00d38
Mohamed Suliman <sulimmoh@gmail.com>
2022-07-11 18:05:49 -0700

    3

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -4,8 +4,8 @@
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
-void _puts(char *str);
 int _putchar(char c);
+void _puts(char *str);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _isupper(int c);
