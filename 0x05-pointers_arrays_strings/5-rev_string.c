39418889417948e98704c888db0f0ac608bb0779
Mohamed Suliman <sulimmoh@gmail.com>
2022-07-11 19:03:42 -0700

    5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -9,15 +9,15 @@
 void rev_string(char *s)
 {
 	signed int long i;
-	int len = strlen(s);
-	char t[len];
+	int l = strlen(s);
+	char t[l];
 	signed int long j;
 
-	for (i = len - 1; i > -1; i--)
+	for (i = l - 1; i > -1; i--)
 	{
-		t [len - 1 - i] = s[i];
+		t [l - 1 - i] = s[i];
 	}
-	for (j = 0; j < len; j++)
+	for (j = 0; j < l; j++)
 	{
 		s[j] = t [j];
 	}
