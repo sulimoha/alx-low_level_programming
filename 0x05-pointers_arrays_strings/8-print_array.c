916fc775e601e18afd5898bd42ffd324a8dcf1ac
Mohamed Suliman <sulimmoh@gmail.com>
2022-07-11 22:10:47 -0700

    9

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strcpy - copies the string pointed to by src, including the terminating
+ * null byte (\0), to the buffer pointed to by dest
+ * @dest : a pointer to a buffer
+ * @src : a pointer to a string
+ *
+ * Return: char
+*/
+char *_strcpy(char *dest, char *src)
+{
+	unsigned int long i;
+
+	for (i = 0; i < strlen(stc); i++)
+	{
+		dest[i] = src[i];
+	}
+	return (*dest);
+
+}
