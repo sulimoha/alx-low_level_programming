f5632bbc4c7455d06181d4472b03dadc530d5e86
Mohamed Suliman <sulimmoh@gmail.com>
2022-07-12 20:02:32 -0700

    10

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -16,10 +16,7 @@ int _atoi(char *s)
 	{
 		if ((int)s[i] == 45)
 		{
-			if ((int)s[i + 1] >= 48 && (int)s[i + 1] <= 57)
-			{
-				sign = -1 * sign;
-			}
+			sign = -1 * sign;
 		}
 	}
 	for (i = 0; i < (int)strlen(s); i++)
