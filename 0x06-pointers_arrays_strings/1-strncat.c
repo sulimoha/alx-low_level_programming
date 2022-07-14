cdba9cb51d8e965d3efc14da35ac785440672e51
Mohamed Suliman <sulimmoh@gmail.com>
2022-07-14 07:46:02 -0700

    1

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *_strncat - concatenates two strings
+ * @dest : a pointer to dest
+ * @src : apointer to src
+ * @n : number of chars in src
+ *
+ * Return: char*
+*/
+char *_strncat(char *dest, char *src, int n)
+{
+	int i;
+	int destLen = 0;
+	/*int srcLen = 0;*/
+
+
+	while (dest[destLen] != '\0')
+	{
+		destLen++;
+	}
+	/*while (src[srcLen] != '\0')
+	{
+		srcLen++;
+	}
+	*/
+	destLen = destLen + n;
+	for (i = 0; i < n; i++)
+	{
+		dest[destLen - n + i] = src[i];
+	}
+	return (dest);
+}
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -6,6 +6,7 @@ void swap_int(int *a, int *b);
 int _strlen(char *s);
 int _putchar(char c);
 char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
 void _puts(char *str);
 void print_rev(char *s);
 void rev_string(char *s);
