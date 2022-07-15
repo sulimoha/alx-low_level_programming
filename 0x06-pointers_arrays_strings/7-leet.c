a232c6b662f6b1e175e10291289ecf4492ad5b7d
Mohamed Suliman <sulimmoh@gmail.com>
2022-07-15 02:25:42 -0700

    7

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -13,15 +13,15 @@ char *leet(char *s)
 	int a;
 	int b;
 	int c;
-	char g[] = {101, 105, 114, 117, 124};
-	char l[] = {52, 51, 49, 48, 55};
+	char g[] = {'A', 'E', 'L', 'O', 'T'};
+	char l[] = {'4', '3', '1', '0', '7'};
 
 	while (s[i] != '\0')
 	{
 		for (j = 0; j < 5; j++)
 		{
 			a = g[j];
-			b = a + 40;
+			b = a - 32;
 			c = l[j];
 			if (s[i] == a || s[i] == b)
 			{
