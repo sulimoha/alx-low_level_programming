cc67892bb0b579140ae05ba29074503b0576fbc6
Mohamed Suliman <sulimmoh@gmail.com>
2022-07-14 23:21:44 -0700

    6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -11,7 +11,7 @@ char *cap_string(char *s)
 	int i = 0;
 	int j = 0;
 	int k = 0;
-	char sep [] = {32, 9, 10, 44, 59, 46, 33, 63, 22, 40, 41, 123, 125}; 
+	char sep [] = {32, 9, 10, 44, 59, 46, 33, 63, 22, 40, 41, 123, 125};
 
 	while (s[i] != '\0')
 	{
@@ -23,7 +23,7 @@ char *cap_string(char *s)
 		{
 			for (k = 0; k < 13; k++)
 			{
-				if(s[j - 1] == sep[k])
+				if (s[j - 1] == sep[k])
 				{
 					s[j] = s[j] - 32;
 				}
