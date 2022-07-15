076d191bcf16311b9e3c294e449c2b79ff3adace
Mohamed Suliman <sulimmoh@gmail.com>
2022-07-15 01:51:54 -0700

    7

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -12,6 +12,7 @@ int _strcmp(char *s1, char *s2);
 void reverse_array(int *a, int n);
 char *string_toupper(char *);
 char *cap_string(char *);
+char *leet(char *);
 void _puts(char *str);
 void print_rev(char *s);
 void rev_string(char *s);
