359336645f1790272da09f3a0411f088afd33558
Mohamed Suliman <sulimmoh@gmail.com>
2022-07-18 19:21:19 -0700

    6

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,32 +1,27 @@
 #include "main.h"
 /**
- *_strstr - locates a substring
- * @haystack : string
- * @needle : substring
+ *print_chessboard - prints the chessboard.
+ * @a : int
  *
- * Return: a pointer to the beginning of the located substring, or NULL
- * if the substring is not found
+ * Return: void
 */
-char *_strstr(char *haystack, char *needle)
+void print_chessboard(char (*a)[8])
 {
 	int i = 0;
-	int j = 0;
-
-	while (haystack[i])
+	int j;
+	int k;
+	
+	while (a[i][8])
 	{
-		while (needle[j])
-		{
-			if (haystack[i + j] == needle[j])
-			{
-				break;
-			}
-			j++;
-		}
-		if (needle[j] == '\0')
-		{
-			return (haystack + i);
-		}
 		i++;
 	}
-	return ('\0');
+	for (k = 0; k < i; k++)
+	{
+		for (j = 0; j < 8; j++)
+		{
+			_putchar(a[k][j]);
+		}
+		_putchar('\n');
+	}
+
 }
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -11,6 +11,7 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
+void print_chessboard(char (*a)[8]);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
