f3788591aecce1818dea71d731392073c3568878
Mohamed Suliman <sulimmoh@gmail.com>
2022-07-19 11:02:42 -0700

    9

---
+++ 0x07-pointers_arrays_strings/101-crackme_password.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ *set_string - sets the value of a pointer to a char
+ * @s : a pointer  a pointer to s
+ * @to: a pointer to size
+ *
+ * Return: void
+*/
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
