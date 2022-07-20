58ca22880a82a3b1791736641cc7747b49bc2b36
Mohamed Suliman <sulimmoh@gmail.com>
2022-07-20 16:43:08 -0700

    4

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -9,6 +9,7 @@ void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
 int factorial(int n);
+int _pow_recursion(int x, int y);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
