cd5092879dbf5d77c80863e5788a007caa31f4bd
Mohamed Suliman <sulimmoh@gmail.com>
2022-07-20 20:14:15 -0700

    5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,4 +1,18 @@
 #include "main.h"
+
+int sqf(int i, int n);
+
+/**
+ *_sqrt_recursion - the natural square root of a number.
+ * @n : a number to find the sqrt for
+ *
+ * Return: int
+*/
+int _sqrt_recursion(int n)
+{
+	return (sqf(0, n));
+}
+
 /**
  *sqf - returns the natural square root of a number startting from zero.
  * @i :  intiial guess
@@ -18,15 +32,4 @@ int sqf(int i, int n)
 		sqf(i, n);
 	}
 	return (-1);
-}
-
-/**
- *_sqrt_recursion - the natural square root of a number.
- * @n : a number to find the sqrt for
- *
- * Return: int
-*/
-int _sqrt_recursion(int n)
-{
-	return (sqf(0, n));
 }
