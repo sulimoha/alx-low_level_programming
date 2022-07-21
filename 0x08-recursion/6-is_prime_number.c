e2822f60991299da936fcb3d0c4167106927e312
Mohamed Suliman <sulimmoh@gmail.com>
2022-07-20 19:53:07 -0700

    6

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -2,17 +2,17 @@
 /**
  *is_prime_number - returns 1 if the input integer
  *is a prime number, otherwise return 0.
- * @n : a number 
+ * @n : a number
  *
  * Return: int
 */
 int is_prime_number(int n)
 {
-	if (n ==0 )
+	if (n == 0)
 	{
 		return (0);
 	}
-	if (n % 1 == 0 && n % n)
+	if (n % 1 == 0 && n % n == 0)
 	{
 		return (1);
 	}
