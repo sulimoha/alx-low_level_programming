23becd5ca8ec1b8854faf7cd6363afebc453eafd
Mohamed Suliman <sulimmoh@gmail.com>
2022-07-22 18:55:30 -0700

    0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main -   prints the program name, followed by a new line
+ * @argc : int
+ * @argv : string
+ *
+ * Return: 0 for success
+*/
+int main(int argc, char* argv[])
+{
+	int a = argc;
+	a = 0;
+	printf("%s\n", argv[0]);
+	return (a);
+}
---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,7 @@
+#ifndef HEADER_H
+#define HEADER_H
+
+int main(int argc, char* argv[]);
+
+#endif
+
