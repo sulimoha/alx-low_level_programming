2238f185c08b4c7e63dd303213dea37ca8fd5460
Mohamed Suliman <sulimmoh@gmail.com>
2022-07-22 19:43:11 -0700

    3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - the number of arguments passed into it.
+ * @argc : int
+ * @argv : string
+ *
+ * Return: 0 for success
+*/
+int main(int argc, char *argv[])
+{
+	int ar1 = 0;
+	int ar2 = 0;
+
+	if (argc == 3)
+	{
+		ar1 = atoi(argv[1]);
+		ar2 = atoi(argv[2]);
+		printf("%d\n", ar1 * ar2);
+	}
+	else
+	{
+		printf("Errot\n");
+	}
+	return (0);
+}
