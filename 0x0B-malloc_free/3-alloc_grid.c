e62cc6de9624e1a753c9770d3a93db5d21e6e90a
Mohamed Suliman <sulimmoh@gmail.com>
2022-07-25 19:00:17 -0700

    3

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -17,7 +17,6 @@ int **alloc_grid(int width, int height)
 
 	if (width <= 0 || height <= 0)
 	{
-		free(t);
 		return (NULL);
 	}
 	t = malloc(height * sizeof(int *));
@@ -31,7 +30,7 @@ int **alloc_grid(int width, int height)
 		t[k] = malloc(width * sizeof(int));
 		if (t[k] == NULL)
 		{
-			free(t);
+			free(t[k]);
 			return (NULL);
 		}
 	}
