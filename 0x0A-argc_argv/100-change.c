6d517940777787e6f07b832e521adb71d36c9f49
Mohamed Suliman <sulimmoh@gmail.com>
2022-07-27 16:06:58 -0700

    5

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -16,7 +16,7 @@ int main(int argc, char *argv[])
 	int b;
 	int ch[5] = {25, 10, 5, 2, 1};
 
-	if (argc != 1)
+	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
@@ -25,6 +25,7 @@ int main(int argc, char *argv[])
 	if (b < 0)
 	{
 		printf("0\n");
+		return (0);
 	}
 
 	for (i = 0; i < 5; i++)
