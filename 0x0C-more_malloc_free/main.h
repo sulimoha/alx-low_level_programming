7b073f1cd04b389758f2c6678b7177a69003289a
Mohamed Suliman <sulimmoh@gmail.com>
2022-07-28 10:03:26 -0700

    0

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,12 +1,6 @@
 #ifndef HEADER_H
 #define HEADER_H
-int _strlen(char *s);
-char *create_array(unsigned int size, char c);
-char *_strdup(char *str);
-char *str_concat(char *s1, char *s2);
-int **alloc_grid(int width, int height);
-void free_grid(int **grid, int height);
-char *argstostr(int ac, char **av);
+void _putchar(char c);
 void *malloc_checked(unsigned int b);
 #endif
 
