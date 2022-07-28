8ac97d4649f4041eed8f0799d8de4c3a8a0b418f
Mohamed Suliman <sulimmoh@gmail.com>
2022-07-28 11:20:57 -0700

    1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -16,6 +16,10 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	char *t;
 	unsigned int len1 = 0, len2 = 0, i = 0, j = 0;
 
+	if (s1 == NULL)
+		s1[0] = '\0';
+	if (s2 == NULL)
+		s2[0] = '\0';
 	if (s1 != NULL)
 		while (s1[len1] != '\0')
 		{
