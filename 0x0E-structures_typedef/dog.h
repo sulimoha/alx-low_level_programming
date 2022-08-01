8de548e1cab56207dea306d6df43555b747246bd
Mohamed Suliman <sulimmoh@gmail.com>
2022-08-01 10:23:52 -0700

    0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef HEADER_H
+#define HEADER_H
+
+/**
+ * struct dog - a dog struct with the follwing elements
+ * @name: name of the dog
+ * @age: Seage of the dog
+ * @owner: owner of the dog
+ *
+ * Description: Longer description (unneeded for this task)
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
