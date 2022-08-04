e88cd5be274d0c88d4490b80d03cc657456fc59d
Mohamed Suliman <sulimmoh@gmail.com>
2022-08-04 07:19:13 -0700

    1

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -14,6 +14,10 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_list ap;
 	unsigned int i;
 
+	if (n == 0)
+	{
+		return;
+	}
 	va_start(ap, n);	/* Initialize the argument list. */
 	for (i = 0; i < n - 1; i++)
 	{
