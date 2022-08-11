cfba41c0b07c6c52b2cbaba7dee4218856958b32
Mohamed Suliman <sulimmoh@gmail.com>
2022-08-11 14:47:23 -0700

    fixed betty feedback

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -2,27 +2,27 @@
 #include <stdlib.h>
 #include "lists.h"
 /**
- * size_t print_list - prints all the elements of a list_t list.
+ *print_list - prints all the elements of a list_t list.
  * @h: list
  *
  * Return: the number of nodes
 */
 size_t print_list(const list_t *h)
 {
-	int n_nodes = 0;
-	
+	size_t nt n_nodes = 0;
+
 	while (h != NULL)
 	{
 		if (h->str == NULL)
 		{
 			printf("[0] (nil)\n");
 		}
-		else 
+		else
 		{
 			printf("[%d] %s\n", h->len, h->str);
 		}
 		h = h->next;
-		n_nodes ++;
+		n_nodes++;
 	}
 	return (n_nodes);
 }
