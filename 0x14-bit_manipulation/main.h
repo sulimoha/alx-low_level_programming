33a08c2a1062af0df9b762265288aa5c1dee4788
Mohamed Suliman <sulimmoh@gmail.com>
2022-08-18 13:59:54 -0700

    Write a function that prints the binary representation of a number.

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -2,6 +2,8 @@
 #define HEADER_H
 #include <stdio.h>
 #include <stdlib.h>
+int _putchar(char c);
+void _rshift(unsigned int long n);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 #endif
