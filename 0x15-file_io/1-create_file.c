06da69ad32db6d1f9afe4da327eb3b48459b751c
Mohamed Suliman <sulimmoh@gmail.com>
2022-08-22 17:53:58 -0700

    fixed the permission

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -33,7 +33,7 @@ int create_file(const char *filename, char *text_content)
 	}
 	if (text_content)
 	{
-		if (write(fd, text_content, text_len) == -1)
+		if (write(fd, text_content, text_len) < 0)
 		{
 			return (-1);
 		}
