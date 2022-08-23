267c4cf70adadb7d9298f75a29a3447f858a3429
Mohamed Suliman <sulimmoh@gmail.com>
2022-08-22 20:38:37 -0700

    Write a program that copies the content of a file to another file.

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -6,7 +6,7 @@
  *
  * Return: 0 on sucess
 */
-int main (int ac, char **av)
+int main(int ac, char **av)
 {
 	char buf[1024];
 	int fd_file_from, fd_file_to;
@@ -24,7 +24,7 @@ int main (int ac, char **av)
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
 		exit(98);
 	}
-	if(fd_file_to == -1)
+	if (fd_file_to == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
 		exit(99);
@@ -37,16 +37,16 @@ int main (int ac, char **av)
 			exit(98);
 		}
 		written = write(fd_file_to, buf, readen);
-		if(written == -1 || readen != written)
+		if (written == -1 || readen != written)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
 			close(fd_file_from);
 			exit(99);
 		}
-	} while(readen);
+	} while (readen);
 	readen = close(fd_file_from);
 	written = close(fd_file_to);
-	if(readen || written)
+	if (readen || written)
 	{
 		if (readen)
 		{
