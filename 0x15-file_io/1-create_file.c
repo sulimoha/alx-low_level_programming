b063f548641fa915fd6d9242d4a01f435804d114
Mohamed Suliman <sulimmoh@gmail.com>
2022-08-22 17:22:56 -0700

    fixed the string length while loop

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -22,13 +22,12 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	}
 
-	while (*text_content != '\0')
+	while (text_content[text_len])
 	{
 		text_len++;
-		text_content++;
 	}
 	text_len += 1;
-	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 00600);
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, S_IWUSR);
 	if (fd == -1)
 	{
 		return (-1);
