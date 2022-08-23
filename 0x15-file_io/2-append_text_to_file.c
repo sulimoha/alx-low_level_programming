245ec64082b18f83b068ce670fa59ebddc13afaa
Mohamed Suliman <sulimmoh@gmail.com>
2022-08-22 18:22:52 -0700

    removed the permission mode

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -27,7 +27,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	{
 		text_len++;
 	}
-	fd = open(filename, O_WRONLY | O_APPEND, 00600);
+	fd = open(filename, O_WRONLY | O_APPEND);
 	if (fd == -1)
 	{
 		return (-1);
