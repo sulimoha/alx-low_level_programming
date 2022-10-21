999b286682e264ef048567fc26ffe5cea856fd79
Mohamed <sulimmoh@gmail.com>
2022-10-21 13:27:38 -0700

    fixed betty

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -13,9 +13,11 @@ hash_table_t *hash_table_create(unsigned long int size)
 
 	if (size < 1)
 		return (NULL);
+
 	ht = malloc(sizeof(hash_table_t));
 	if (ht == NULL)
 		return (NULL);
+
 	ht->size = size;
 	ht->array = calloc(size, sizeof(hash_node_t *));
 	if (ht->array == NULL)
--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -10,6 +10,8 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	int idx;
 	hash_node_t *node;
+
+
 	if (key == NULL || strlen(key) == 0 || ht == NULL)
 		return (0);
 	idx = key_index((const unsigned char *) key, ht->size);
