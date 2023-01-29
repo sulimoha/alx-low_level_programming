4d71a4cb906980cb69a230856ee84c833dcb4342
Mohamed <sulimmoh@gmail.com>
2023-01-29 16:50:18 -0600

    Write a function that searches for a value in a sorted array of integers using the Binary search algorithm

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -4,25 +4,8 @@
 #include <math.h>
 #include <stdlib.h>
 
-/* helpers */
-void print_array(int *array, size_t inc, size_t size);
-
-typedef struct skiplist_s
-{
-				int n;
-				size_t index;
-				struct skiplist_s *next;
-				struct skiplist_s *express;
-} skiplist_t;
-
-typedef struct listint_s
-{
-		int n;
-		size_t index;
-		struct listint_s *next;
-} listint_t;
 
 /* main functions */
 int linear_search(int *array, size_t size, int value);
-
+int binary_search(int *array, size_t size, int value);
 #endif
